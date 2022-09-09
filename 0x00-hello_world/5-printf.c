8d21597592dcdcf7ef246db430428dfb7de4ae23
Brenden Asare <brendenasare@gmail.com>
2022-09-08 22:20:44 -0700

    printfff

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
-#include <stdio.h> 
+#include <stdio.h>
+
 /**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
+ * main - Entry point of code
+ *
+ * Return: Always 0 (success)
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	prinf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
