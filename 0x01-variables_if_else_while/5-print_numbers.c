797d4f11d99ab9ce93397e2fca8fea3e28dd29a5
Brenden Asare <brendenasare@gmail.com>
2022-09-09 21:50:31 -0700

    thiss again

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - print if the muber is positive, zero, or negativee
- * 
- * Description: using the main function
- * this program prints "Programming is positive, zero, or negative
- * Return: 0
- */
+* main - print if the muber is positive, zero, or negativee
+* 
+* Description: using the main function
+* this program prints "Programming is positive, zero, or negative
+* Return: 0
+*/
 int main(void)
 {
 int i;
