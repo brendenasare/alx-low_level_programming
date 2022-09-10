b4c2562516bacfb68d48e4fb78fb08d5d12b640c
Brenden Asare <brendenasare@gmail.com>
2022-09-09 21:38:11 -0700

    againnn

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#inlcude <stdio.h>
+/**
+ * main - print if the muber is positive, zero, or negativee
+ * 
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+	printf("%i", i);
+}
+printf("\n");
+return (0);
+}
