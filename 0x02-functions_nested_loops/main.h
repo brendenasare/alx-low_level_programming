f6c043336799991dd565bb20267156ff82ab9b5c
Brenden Asare <brendenasare@gmail.com>
2022-09-14 03:13:43 -0700

    hiju

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef _HOLBERTON_H_INCLUDED
+#define _HOLBERTON_H_INCLUDED
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -11,5 +11,10 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int a, int b);
 void print_to_98(int n);
+void print_times_table(int n);
+void print_times_table(int n);
+void num(int r, int n);
+
+#endif
